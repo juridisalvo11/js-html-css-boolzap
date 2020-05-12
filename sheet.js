ae6af49f6926077f83e3defa47a50fd01ed1854a
Juri Di salvo <Juridisalvo11@gmail.com>
2020-05-12 16:22:07 +0200

    Boolzap - milestone 2 da sistemare

--- sheet.js
+++ sheet.js
@@ -2,25 +2,78 @@
 $('.microphone > i').click(function() {
     //leggo il testo del messaggio inserito dall'utente
     var messaggio_ricevuto = $('.insert-message').val();
-    //Inserisco il testo letto dall'input
-    $('.template .message-text').text(messaggio_ricevuto);
-    //Copio l'elemento template
-    var current_message = $('.template .received').clone();
-    //Aggiungo il nuovo messaggio alla chat con il comando append
-    $('.chat-container').append(current_message);
+    if (messaggio_ricevuto != '') {
+        //Copio l'elemento template
+        var current_message = $('.template .message').clone();
+        //Inserisco il testo letto dall'input
+        current_message.find('.message-text').text(messaggio_ricevuto);
+        //Aggiungo classe received per dare lo stile al messaggio ricevuto
+        current_message.addClass('received');
+        //Aggiungo il nuovo messaggio alla chat con il comando append
+        $('.chat-container').append(current_message);
+        //Svuoto l'input
+        $('.insert-message').val('');
+
+        setTimeout(function(){
+            var answer_message = $('.template .message').clone();
+
+            answer_message.find('.message-text').text('Ok')
+
+            answer_message.addClass('sent');
+
+            $('.chat-container').append(answer_message);
+
+        }, 1000);
+    }
+
+
+
 
 })
 
 $('.insert-message').keypress(function(event) {
     var key = event.which;
     if (key == 13) {
-        //leggo il testo del messaggio inserito dall'utente
-        var messaggio_ricevuto = $('.insert-message').val();
-        //Inserisco il testo letto dall'input
-        $('.template .message-text').text(messaggio_ricevuto);
-        //Copio l'elemento template
-        var current_message = $('.template .received').clone();
-        //Aggiungo il nuovo messaggio alla chat con il comando append
-        $('.chat-container').append(current_message);
+        send_message()
     }
 })
+
+//Intercetto click dell'utente sul tasto Cerca
+$('.search-message').click(function() {
+    //vado a leggere ciò che l'utente ha scritto nell'input
+    var search = $('.start-chat').val().trim();
+    //controllo se lìutente ha scritto qualcosa
+    if (search != '') {
+        //Vado a controllare ogni elemeneto in lista per vedere se c'è una corrispondenza con quello digitato dall'lìutente
+        $('.name').each(function() {
+            var chat_name = $(this).text();
+
+            if (chat_name == search) {
+                $(this).show();
+
+            } else {
+                $(this).hide();
+            }
+        })
+    } else {
+        $('.name').show();
+    }
+
+})
+
+function send_message() {
+    //leggo il testo del messaggio inserito dall'utente
+    var messaggio_ricevuto = $('.insert-message').val();
+    if (messaggio_ricevuto != '') {
+        //Copio l'elemento template
+        var current_message = $('.template .message').clone();
+        //Inserisco il testo letto dall'input
+        current_message.find('.message-text').text(messaggio_ricevuto);
+        //Aggiungo classe received per dare lo stile al messaggio ricevuto
+        current_message.addClass('received');
+        //Aggiungo il nuovo messaggio alla chat con il comando append
+        $('.chat-container').append(current_message);
+        //Svuoto l'input
+        $('.insert-message').val('');
+    }
+}
